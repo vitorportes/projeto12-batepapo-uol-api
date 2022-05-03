2d4598ec0dc00f7ac530da0077d5704a52ab9007
Vitor Portes <vitorportnoy@gmail.com>
2022-05-03 14:08:53 -0300

    mongoclient

---
+++ index.js
@@ -0,0 +1,41 @@
+import express, { json } from 'express';
+import cors from 'cors';
+import chalk from 'chalk';
+import Joi from 'joi';
+import dayjs from 'dayjs';
+import { stripHtml } from 'string-strip-html';
+import { MongoClient, ObjectId } from 'mongodb';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const authSchema = Joi.object({
+  name: Joi.string().required(),
+});
+
+const messageBodySchema = Joi.object({
+  to: Joi.string().required(),
+  text: Joi.string().required(),
+  type: Joi.string().valid('message', 'private_message').required(),
+});
+
+const app = express();
+app.use(cors());
+app.use(json());
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db = null;
+
+mongoClient
+  .connect()
+  .then(() => {
+    db = mongoClient.db('batepapo-uol');
+    console.log(chalk.bold.red('Conected to MongoDB'));
+  })
+  .catch((err) => {
+    console.log(chalk.bold.red('Error connecting to MongoDB', err));
+  });
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(chalk.bold.green(`Server running on port ${port}`));
+});
